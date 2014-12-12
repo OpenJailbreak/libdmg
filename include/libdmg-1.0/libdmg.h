62b64aa7f4d5e60dace84e810a078b10cb8f9c51
Joshua Hill <posixninja@gmail.com>
2014-12-12 15:38:07 -0500

    Added DMG code from xpwn. I'll rework this in a seperate branch also. This is a checkpoint before I start building

--- include/libdmg-1.0/libdmg.h
+++ include/libdmg-1.0/libdmg.h
@@ -8,6 +8,11 @@
 #ifndef LIBDMG_H_
 #define LIBDMG_H_
 
-
+#include <libdmg-1.0/abstractfile.h>
+#include <libdmg-1.0/common.h>
+#include <libdmg-1.0/dmg.h>
+#include <libdmg-1.0/dmgfile.h>
+#include <libdmg-1.0/dmglib.h>
+#include <libdmg-1.0/filevault.h>
 
 #endif /* LIBDMG_H_ */
